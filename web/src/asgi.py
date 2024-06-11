5f080f0086f3683ce9e6838c8b0d33e69b5be8a8
Сергей Кимайкин <017Alykar1990@mail.ru>
2024-06-12 04:37:45 +1000

    hell_v2

--- web/src/asgi.py
+++ web/src/asgi.py
@@ -1,6 +1,6 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
+# from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
@@ -10,6 +10,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from chat.routing import websocket_urlpatterns
+from chat.middleware import AuthMiddlewareStack
 
 # пиздец
 application = ProtocolTypeRouter(
