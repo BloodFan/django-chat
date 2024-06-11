032a7b840aa8a967acc278af2823f0f50fa18466
Сергей Кимайкин <017Alykar1990@mail.ru>
2024-06-12 01:28:15 +1000

    asgi_v2

--- web/src/asgi.py
+++ web/src/asgi.py
@@ -1,13 +1,15 @@
 import os
 
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+django.setup()
 
 django_asgi_app = get_asgi_application()
-
 
 from chat.middleware import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
